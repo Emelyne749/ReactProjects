5618be2c4da17d951b66e0075c5eb5abf021bb48
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-03-02 18:22:46 +0200

    project init

---
+++ ReactFunFact/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-purple-600 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind v4 is working 🚀
+      </h1>
+    </div>
+  )
+}
