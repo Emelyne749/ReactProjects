ac2bdba6d4c5d7dd735758aa9fd02da614077a11
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-03-03 19:40:16 +0200

    complete react fanfact project

--- ReactFunFact/src/components/Main.jsx
+++ ReactFunFact/src/components/Main.jsx
@@ -1,15 +1,17 @@
+import halfLogo from '../assets/halfLogo.png';
+
 export default function Main() {
   return (
-    <>
-        <h1 class = "text-4xl font-bold text-white text-center p-10">Fun Facts about React</h1>
-        <ul class="text-gray-300 pr-10 pl-15 ml-5 list-disc">
-            <li class="marker:text-blue-300 marker:text-3xl">Was first released in 2013</li>
-            <li class="marker:text-blue-300 marker:text-3xl">Was originally created by Jordan Walke</li>
-            <li class="marker:text-blue-300 marker:text-3xl">Has well over 200K stars on GitHub</li>
-            <li class="marker:text-blue-300 marker:text-3xl">Is maintained by Meta</li>
-            <li class="marker:text-blue-300 marker:text-3xl">Powers thousands of enterprise apps, including mobile apps</li>
+    <div className="bg-cover bg-center" style={{ backgroundImage: `url(${halfLogo})`, backgroundRepeat: `no-repeat`, backgroundPosition: `right 70%` , backgroundSize: `150px`}}>
+        <h1 className="text-4xl font-bold text-white text-center p-10">Fun Facts about React</h1>
+        <ul className="text-gray-300 pr-10 pl-15 ml-5 list-disc">
+            <li className="marker:text-blue-300 marker:text-3xl">Was first released in 2013</li>
+            <li className="marker:text-blue-300 marker:text-3xl">Was originally created by Jordan Walke</li>
+            <li className="marker:text-blue-300 marker:text-3xl">Has well over 200K stars on GitHub</li>
+            <li className="marker:text-blue-300 marker:text-3xl">Is maintained by Meta</li>
+            <li className="marker:text-blue-300 marker:text-3xl">Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </>
+    </div>
       
   )
 }
--- ReactFunFact/src/components/navBar.jsx
+++ ReactFunFact/src/components/navBar.jsx
@@ -1,9 +1,9 @@
 import logo from '../assets/logo.png'
 
 export default function NavBar() {
-  return (
-      <nav class ="bg-gray-900 p-10">
-         <img src={logo} alt="logo" class="w-60 h-10"/>
+    return (
+      <nav className ="bg-gray-900 p-10">
+       <img src={logo} alt="logo" className="w-60 h-10"/>
       </nav>
-  )
+    )
 }
