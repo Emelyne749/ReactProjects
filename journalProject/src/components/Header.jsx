e58245ddf3416d4bdf5c86632e3f41368d496cf6
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-03-05 21:45:55 +0200

    Added the global icon

--- journalProject/src/components/Header.jsx
+++ journalProject/src/components/Header.jsx
@@ -3,9 +3,9 @@ import path from "../assets/Path.png"
 export default function Header() {
     return (
         <>
-            <div className="bg-red-500 flex justify-center h-10">
-                <img src={path} alt="Globe icon" className="w-10 h-10"/>
-                <h1 className="text-gray-200 text-semi-bold">my travel journal.</h1>
+            <div className="bg-red-500 flex justify-center h-15 p-3 gap-3">
+                <img src={path} alt="Globe icon" className="w-9 h-9 p-1"/>
+                <h1 className="text-gray-200 text-semi-bold p-2">my travel journal.</h1>
             </div>
         </>
     )
