a660e134087c231f722f3f717428ebea447063a3
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-03-05 18:38:56 +0200

    journal project initialization

---
+++ journalProject/src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./components/Main"
+import NavBar from "./components/navBar"
+
+export default function App() {
+  return (
+      <>
+      <div className="w-4xs h-120 bg-gray-700 ">
+        <NavBar/> 
+        <Main/>
+      </div>
+
+      </>
+  )
+}
